022455941de04b98cc22a393c28f1ad6c446bc12
Vivek Teega <vivek.teega@gmail.com>
2024-07-26 22:39:10 +0530

    Added proper Terraform port configuration for Testnet and mainnet

--- terraform/main.tf
+++ terraform/main.tf
@@ -3,8 +3,8 @@ provider "aws" {
 }
 
 resource "aws_key_pair" "deployer_1" {
-  key_name   = "ghost_server_key_1"
-  public_key = file("../auth/ghost_server_key_1.pub")
+  key_name   = "blockbook_key"
+  public_key = file("../auth/blockbook_key.pub")
 }
 
 resource "aws_security_group" "blockbook_sg" {
@@ -18,13 +18,59 @@ resource "aws_security_group" "blockbook_sg" {
     cidr_blocks = ["0.0.0.0/0"]
   }
 
+  # Mainnet Ports
   ingress {
-    from_port   = 2368
-    to_port     = 2368
+    from_port   = 8066
+    to_port     = 8066
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+    from_port   = 38366
+    to_port     = 38366
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+    from_port   = 9066
+    to_port     = 9066
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+    from_port   = 9166
+    to_port     = 9166
     protocol    = "tcp"
     cidr_blocks = ["0.0.0.0/0"]
   }
 
+  # Testnet Ports
+  ingress {
+    from_port   = 18066
+    to_port     = 18066
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+    from_port   = 48366
+    to_port     = 48366
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+    from_port   = 19066
+    to_port     = 19066
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+  ingress {
+    from_port   = 19166
+    to_port     = 19166
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]
+  }
+
+  # ICMP for Ping
   ingress {
     from_port   = -1
     to_port     = -1
@@ -40,7 +86,7 @@ resource "aws_security_group" "blockbook_sg" {
   }
 }
 
-resource "aws_instance" "app_server" {
+resource "aws_instance" "blockbook_server" {
   ami           = "ami-07c8c1b18ca66bb07"
   instance_type = "t3.micro"
   key_name      = aws_key_pair.deployer_1.key_name
@@ -54,6 +100,6 @@ resource "aws_instance" "app_server" {
   }
 }
 
-output "instance_ip" {
-  value = aws_instance.app_server.public_ip
+output "blockbook_instance_ip" {
+  value = aws_instance.blockbook_server.public_ip
 }
